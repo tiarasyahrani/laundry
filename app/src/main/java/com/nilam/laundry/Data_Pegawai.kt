59ecdf50feb0fd2ddfa22e30a1ce752b960ab716
tiarasyahrani <tiaranilam1178@gmail.com>
2025-02-05 10:26:59 +0700

    membuat tambah pegawai & translation bahasa tambah pegawai

---
+++ app/src/main/java/com/nilam/laundry/Data_Pegawai.kt
@@ -0,0 +1,20 @@
+package com.nilam.laundry
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Data_Pegawai : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_data_pegawai)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
